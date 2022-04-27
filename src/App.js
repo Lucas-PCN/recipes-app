12070b79ed476ab89b69ac1f2053f3be738f80c7
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-04-27 17:33:50 -0300

    Cumpre requisito 9

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Provider>
-          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+          {/* <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
           <Route exact path="/done-recipes" component={ DoneRecipes } />
           <Route exact path="/profile" component={ Profile } />
           <Route
@@ -36,7 +36,7 @@ function App() {
           />
           <Route exact path="/explore/drinks" component={ ExploreDrinks } />
           <Route exact path="/explore/foods" component={ ExploreFoods } />
-          <Route exact path="/explore" component={ Explore } />
+          <Route exact path="/explore" component={ Explore } /> */}
           <Route
             exact
             path="/drinks/:id_da_receita/in-progress"
