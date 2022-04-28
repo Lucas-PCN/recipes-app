78d198942bce595dcb9d7d49c16aa6b933da41bf
Alexandre Sullivan Teixeira <ast.sullivan@gmail.com>
2022-04-28 17:37:24 -0300

    Correções Lint e Conflitos

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explorer() {
-  // const history = useHistory();
+  // const history = useHistory()*;
   return (
     <div>
       <Header title="Explore" shouldRenderMagnifier={ false } />
