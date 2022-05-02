e4f36bf71d4050c611184127936f4f326cc1325f
hw1te <gutxander@gmail.com>
2022-05-02 16:22:17 -0300

    [Group 27] Adição dos botões e funcionalidades Botões inseridos de acordo com o test em ambas páginas Redirecionamento realizado nos botões

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import Footer from '../components/Footer';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExplorerFoods() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explore Foods" shouldRenderMagnifier={ false } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
       >
         By Ingredient
       </button>
       <button
         type="button"
         data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
       >
         By Nationality
       </button>
