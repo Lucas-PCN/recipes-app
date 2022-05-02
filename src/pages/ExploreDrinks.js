99362d3447983f634c0e887d82c340413e7f6b9a
hw1te <gutxander@gmail.com>
2022-05-02 15:32:38 -0300

    [Group 27] Finalização da Explore Main page Adição dos botões food e drinks e data-test-id's corretos

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -6,6 +6,24 @@ function ExploreDrinks() {
   return (
     <div>
       <Header title="Explore Drinks" shouldRenderMagnifier={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
