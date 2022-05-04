eb982a888dc695a96e56c01311eb6dc9178ddcbb
hw1te <gutxander@gmail.com>
2022-05-04 14:49:25 -0300

    Lint corrigido.

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -33,7 +33,6 @@ function Provider({ children }) {
     fetchSearchFoods();
   }, []);
 
-
   async function fetchSearchByName(name) {
     const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
 
