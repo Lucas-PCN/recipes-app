0033378495237f9e22eaa12d96e23e680951f07e
hw1te <gutxander@gmail.com>
2022-05-05 16:17:30 -0300

    Desenvolvimento dos requisitos 33 e 34.

--- src/pages/DrinkDetails.js
+++ src/pages/DrinkDetails.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import MyContext from '../context/MyContext';
 
 function DrinkDetails() {
+  const { setResultDataDrinks, resultDataDrinks } = useContext(MyContext);
+
+  async function drinksIdAPI(id) {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    setResultDataDrinks(data);
+  }
+  drinksIdAPI();
+
   return (
-    <h1>Detalhes da bebida selecionada</h1>
+    <>
+      <img
+        data-testid="recipe-photo"
+        alt="Recipe result"
+      />
+      <h1 data-testid="recipe-title">Title a ser inserido</h1>
+      <button
+        type="button"
+        data-testid="share-btn"
+        alt="Share button"
+      >
+        <img src={ shareIcon } alt="shareIcon" />
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        alt="favorite button"
+      >
+        <img src={ whiteHeartIcon } alt="whiteHeartIcon" />
+      </button>
+      <p data-testid="recipe-category">Texto a ser inserido</p>
+      <ul>
+        Ingredients
+        {
+          resultDataDrinks.map((index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              teste
+            </li>
+          ))
+        }
+      </ul>
+      <p data-testid="instructions">Instructions a serem inseridas</p>
+      {
+        resultDataDrinks.map((index) => (
+          <img
+            key={ index }
+            data-testid={ `${index}-recomendation-card` }
+            alt="drink"
+          />
+        ))
+      }
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+        alt="Start recipe"
+      >
+        Start recipe
+      </button>
+    </>
   );
 }
 
--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import MyContext from '../context/MyContext';
 
 function FoodDetails() {
+  const { setResultDataMeals, resultDataMeals } = useContext(MyContext);
+
+  async function mealsIdAPI(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    setResultDataMeals(data);
+  }
+  mealsIdAPI();
+
   return (
-    <h1>Detalhes da receita selecionada</h1>
+    <>
+      <img
+        data-testid="recipe-photo"
+        alt="Recipe result"
+      />
+      <h1 data-testid="recipe-title">Title a ser inserido</h1>
+      <button
+        type="button"
+        data-testid="share-btn"
+        alt="Share button"
+      >
+        <img src={ shareIcon } alt="shareIcon" />
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        alt="favorite button"
+      >
+        <img src={ whiteHeartIcon } alt="whiteHeartIcon" />
+      </button>
+      <p data-testid="recipe-category">Texto a ser inserido</p>
+      <ul>
+        Ingredients
+        {
+          resultDataMeals.map((index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              teste
+            </li>
+          ))
+        }
+      </ul>
+      <p data-testid="instructions">Instructions a serem inseridas</p>
+      {/* Inserir tag vídeo aqui */}
+      {
+        resultDataMeals.map((index) => (
+          <img
+            key={ index }
+            data-testid={ `${index}-recomendation-card` }
+            alt="drink"
+          />
+        ))
+      }
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+        alt="Start recipe"
+      >
+        Start recipe
+      </button>
+    </>
   );
 }
 
